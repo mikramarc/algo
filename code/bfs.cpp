a11c496b084798aa21215d053ec620feeafc22d8
mikramarc <mi.kramarc@gmail.com>
2022-01-03 14:12:25 +0000

    implemented bfs and dfs

---
+++ code/bfs.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+#include <list>
+#include <vector>
+
+typedef std::map<int, std::list<int> > adj_list;
+
+void BFS(const adj_list &graph, int start)
+{
+    std::vector<int> levels(graph.size(), -1);
+    std::vector<bool> visited(graph.size(), false);
+    int current_node;
+    std::list<int> queue;
+
+    visited[start] = true;
+    levels[start] = 0;
+    queue.push_back(start);
+
+    while (!queue.empty())
+    {
+        current_node = queue.front();
+        queue.pop_front();
+        for (const auto& el : graph.at(current_node))
+        {
+            if (!visited[el])
+            {
+                int curr_level = levels[current_node] + 1;
+                std::cout << "node: " << el << " (level: " << curr_level << ")" << std::endl;
+                queue.push_back(el);
+                visited[el] = true;
+                levels[el] = curr_level;
+            }
+        }
+    }
+}
+
+int main()
+{
+    adj_list graph;
+
+    graph.insert(std::pair<int,std::list<int> >(0, {1, 2, 4}));
+    graph.insert(std::pair<int,std::list<int> >(1, {5}));
+    graph.insert(std::pair<int,std::list<int> >(2, {6}));
+    graph.insert(std::pair<int,std::list<int> >(3, {}));
+    graph.insert(std::pair<int,std::list<int> >(4, {3}));
+    graph.insert(std::pair<int,std::list<int> >(5, {6}));
+    graph.insert(std::pair<int,std::list<int> >(6, {7, 8}));
+    graph.insert(std::pair<int,std::list<int> >(7, {}));
+    graph.insert(std::pair<int,std::list<int> >(8, {9}));
+    graph.insert(std::pair<int,std::list<int> >(9, {}));
+
+    BFS(graph, 0);
+
+    return 0;
+}
---
+++ code/dfs.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+#include <list>
+#include <vector>
+
+typedef std::map<int, std::list<int> > adj_list;
+
+// TODO: Write recursive version too
+
+void DFS(const adj_list &graph, int start)
+{
+    int curr_node;
+    std::vector<int> stack;
+    std::vector<bool> visited(graph.size(), false);
+    std::vector<int> depths(graph.size(), -1);
+
+    visited[start] = true;
+    depths[start] = 0;
+    stack.push_back(start);
+
+    while(!stack.empty())
+    {
+        curr_node = stack.back();
+        std::cout << "node: " << curr_node << " (level: " << depths[curr_node] << ")" << std::endl;
+        stack.pop_back();
+
+        for (const auto& el : graph.at(curr_node))
+        {
+            if (!visited[el])
+            {
+                stack.push_back(el);
+                visited[el] = true;
+                depths[el] = depths[curr_node] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    adj_list graph;
+
+    graph.insert(std::pair<int,std::list<int> >(0, {1, 2, 4}));
+    graph.insert(std::pair<int,std::list<int> >(1, {5}));
+    graph.insert(std::pair<int,std::list<int> >(2, {6}));
+    graph.insert(std::pair<int,std::list<int> >(3, {}));
+    graph.insert(std::pair<int,std::list<int> >(4, {3}));
+    graph.insert(std::pair<int,std::list<int> >(5, {6}));
+    graph.insert(std::pair<int,std::list<int> >(6, {7, 8}));
+    graph.insert(std::pair<int,std::list<int> >(7, {}));
+    graph.insert(std::pair<int,std::list<int> >(8, {9}));
+    graph.insert(std::pair<int,std::list<int> >(9, {}));
+
+    DFS(graph, 0);
+
+    return 0;
+}
